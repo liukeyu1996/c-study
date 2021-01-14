2777ed4b73899a1d047130258902d1c7ba9d7539
lky <1806635495@qq.com>
2021-01-14 13:49:04 +0800

    旧版本c++要对某些函数声明

--- 20210106/main.cpp
+++ 20210106/main.cpp
@@ -2,6 +2,7 @@
 using namespace std; //用来声明的命名空间
 
 int main() {//所有的c++程序包含main且返回整数
+    int max(int x,int y); //旧的c++语法，在max出现在main后需要声明，编译从前往后扫描文件
     int a,b,sum;
     cin>>a>>b;//cin输入对象，赋值；>>提取运算符
     sum =a+b;
