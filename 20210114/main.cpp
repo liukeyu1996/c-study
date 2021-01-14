30b51ab6c5c44e1020273b5900e67babdbcbf4eb
lky <1806635495@qq.com>
2021-01-14 15:04:34 +0800

    类，私有共有，成员变量和成员函数，对象声明

--- 20210114/main.cpp
+++ 20210114/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
+//申明一个类
+class Student //声明一个类
+{
+private://设置私有部分
+    int num; //私有变量
+    int score;
+public://类中共有部分
+    void setdata() //定义公用函数
+    {
+        cin>>num;
+        cin>>score;
+    }
+
+    void display()
+    {
+        cout<<"num="<<num<<endl;
+        cout<<"score="<<score<<endl;
+    }
+
+};
+
+Student stud1,stud2; //声明对象
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    stud1.setdata();
+    stud2.setdata();
+    stud1.display();
+    stud2.display();
     return 0;
 }
