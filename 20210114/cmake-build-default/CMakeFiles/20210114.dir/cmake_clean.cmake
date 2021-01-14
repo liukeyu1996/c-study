9593f93247ceac97aed50edacce74c8cfd4f7077
lky <1806635495@qq.com>
2021-01-14 14:34:37 +0800

    类与对象

---
+++ 20210114/cmake-build-default/CMakeFiles/20210114.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "20210114.exe"
+  "20210114.exe.manifest"
+  "20210114.pdb"
+  "CMakeFiles/20210114.dir/main.cpp.obj"
+  "lib20210114.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20210114.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
